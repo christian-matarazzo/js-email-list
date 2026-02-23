3ccf6ec7bd10a7ecefc598a7561b6615bd9092a9
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-02-23 14:10:56 +0100

    add random gen fix

--- main.js
+++ main.js
@@ -5,17 +5,17 @@
 let generateEmail = []
 
 /* start API */
+/* cycle 10 time */
+for(i=0; i<10; i++)
 fetch("https://flynn.boolean.careers/exercises/api/random/mail")
 .then(emails => emails.json())
 .then(email => {
-        console.log(email.response);
-        
-        
-/* cycle 10 time */
-        for(i=0; i<10; i++)
+        /* console.log(email.response); */
 
-            generateEmail.push(email.response) /* push in the empty array */
-
-            console.log(generateEmail);
+ generateEmail.push(email.response) /* push in the empty array */
+ if (generateEmail.length===10){
+    console.log(generateEmail);
+    
+ }
 
     })
